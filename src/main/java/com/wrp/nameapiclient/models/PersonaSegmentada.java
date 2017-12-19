8bdbeaf02cfe56d3edcb60c6673d9f0ff693ff54
ulises <beltrangomezulises@gmail.com>
2017-12-19 15:16:29 -0700

    init

---
+++ src/main/java/com/wrp/nameapiclient/models/PersonaSegmentada.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wrp.nameapiclient.models;
+
+/**
+ *
+ * @author Ulises Beltrán Gómez --- beltrangomezulises@gmail.com, at 21/11/2017
+ */
+public class PersonaSegmentada {
+
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+
+    public PersonaSegmentada() {
+    }
+
+    public PersonaSegmentada(String primerNombre, String segundoNombre, String primerApellido, String segundoApellido) {
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+    
+    
+}
